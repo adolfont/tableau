9a30c1b1c2dffa170ad3fe3a2c1ae25b190ea32f
Adolfo Neto <adolfo.usp@gmail.com>
2020-02-29 19:39:44 -0300

    partial implementation of branching

--- lib/tableau.ex
+++ lib/tableau.ex
@@ -60,15 +60,53 @@ defmodule Tableau do
     %{proof | formulas: apply_all_linear_recursively(proof.formulas)}
   end
 
+
+  defp branching_formula_filter({:t, {_, :or, _}}) do
+    true
+  end
+  defp branching_formula_filter({:f, {_, :and, _}}) do
+    true
+  end
+  defp branching_formula_filter({:t, {_, :implies, _}}) do
+    true
+  end
+
+  defp branching_formula_filter(_), do: false
+
+  def get_branching_formulas(formulas) do
+    Enum.filter(formulas, &branching_formula_filter/1)
+  end
+
+
   def  branch_if_not_closed(proof = %Proof{status: :closed}) do
     proof
   end
 
   def  branch_if_not_closed(proof) do
-    branching_formulas = get_branching_formulas(proof.formulas)
-    proof
+    apply_branching_rule(proof, get_branching_formulas(proof.formulas))
   end
 
+  def apply_branching_rule(proof, []) do
+    proof
+  end
+  def apply_branching_rule(proof, [first_branching_formula |
+  remaining_branching_formulas]) do
+    apply_beta(first_branching_formula)
+    expand_with_branching_rule(proof, apply_beta(first_branching_formula), remaining_branching_formulas)
+  end
+  defp apply_beta({:t, {left, :or, right}}) do
+    [{:t, left}, {:t, right}]
+  end
+  defp apply_beta({:f, {left, :and, right}}) do
+    [{:f, left}, {:f, right}]
+  end
+  defp apply_beta({:t, {left, :implies, right}}) do
+    [{:f, left}, {:t, right}]
+  end
+
+  def expand_with_branching_rule(proof,_,_) do
+    proof
+  end
   def prove_aux(proof) do
     proof
     |> apply_linear_rules()
