82f1a3613789104eeb99c79897fd0a0bf9a90659
Adolfo Neto <adolfo.usp@gmail.com>
2021-02-17 19:13:12 -0300

    Functions for creating valuations with as many atoms as needed

--- lib/valuation/valuation.ex
+++ lib/valuation/valuation.ex
@@ -1,9 +1,18 @@
 defmodule Valuation do
   @truth_values [:t, :f]
 
+  def new() do
+    Map.new()
+  end
+
   def new(formula, truth_value)
       when truth_value in @truth_values do
     %{formula => truth_value}
+  end
+
+  def add(valuation, formula, truth_value)
+      when truth_value in @truth_values do
+    Map.put(valuation, formula, truth_value)
   end
 
   def value(valuation, atom) when is_atom(atom) do
--- test/valuation/valuation_test.exs
+++ test/valuation/valuation_test.exs
@@ -1,10 +1,47 @@
 defmodule ValuationTest do
   use ExUnit.Case
 
+  test "Create an empty valuation" do
+    v = Valuation.new()
+
+    p = Formula.new(:atomic, :p)
+    assert Valuation.value(v, p) == nil
+  end
+
   test "Create a valuation for a formula" do
     p = Formula.new(:atomic, :p)
     v = Valuation.new(p, :t)
 
     assert Valuation.value(v, p) == :t
+  end
+
+  test "Create a valuation for two formulas" do
+    p = Formula.new(:atomic, :p)
+    q = Formula.new(:atomic, :q)
+    v = Valuation.new(p, :t)
+    v = Valuation.add(v, q, :f)
+
+    assert Valuation.value(v, p) == :t
+    assert Valuation.value(v, q) == :f
+
+    # changes the valuation
+    v = Valuation.add(v, p, :f)
+    assert Valuation.value(v, p) == :f
+  end
+
+  test "Create a valuation for three formulas" do
+    p = Formula.new(:atomic, :p)
+    q = Formula.new(:atomic, :q)
+    r = Formula.new(:atomic, :r)
+
+    v =
+      Valuation.new()
+      |> Valuation.add(p, :t)
+      |> Valuation.add(q, :f)
+      |> Valuation.add(r, :t)
+
+    assert Valuation.value(v, p) == :t
+    assert Valuation.value(v, q) == :f
+    assert Valuation.value(v, r) == :t
   end
 end
