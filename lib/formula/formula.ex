bbdfed50d8e6c17f56be0cb4fa913b9770e6542a
Adolfo Neto <adolfo.usp@gmail.com>
2021-02-17 18:29:46 -0300

    Added function to verify if an expression is a well formed formula

--- lib/formula/formula.ex
+++ lib/formula/formula.ex
@@ -1,9 +1,29 @@
 defmodule Formula do
+  @binary_connectives [:and, :or, :implies]
+  @unary_connectives [:not]
+
   def new(:atomic, atom) when is_atom(atom) do
     atom
   end
 
-  def new(:not, formula) do
-    {:not, formula}
+  def new(connective, formula) when connective in @unary_connectives do
+    {connective, formula}
   end
+
+  def new(left_subformula, connective, right_subformula)
+      when connective in @binary_connectives do
+    {left_subformula, connective, right_subformula}
+  end
+
+  def well_formed_formula?(x) when is_atom(x), do: true
+
+  def well_formed_formula?({connective, x})
+      when connective in @unary_connectives,
+      do: well_formed_formula?(x)
+
+  def well_formed_formula?({x, connective, y})
+      when connective in @binary_connectives,
+      do: well_formed_formula?(x) and well_formed_formula?(y)
+
+  def well_formed_formula?(_), do: false
 end
--- test/formula/formula_test.exs
+++ test/formula/formula_test.exs
@@ -13,4 +13,19 @@ defmodule FormulaTest do
 
     assert Formula.new(:not, not_p) == {:not, {:not, :p}}
   end
+
+  test "Creating new binary formulas" do
+    p = Formula.new(:atomic, :p)
+    q = Formula.new(:atomic, :q)
+    assert Formula.new(p, :and, q) == {:p, :and, :q}
+    assert Formula.new(p, :or, q) == {:p, :or, :q}
+    assert Formula.new(p, :implies, q) == {:p, :implies, :q}
+  end
+
+  test "Verifies if an expression is a well-formed formula" do
+    p = Formula.new(:atomic, :p)
+    q = Formula.new(:atomic, :q)
+    assert Formula.well_formed_formula?(Formula.new(p, :and, q))
+    refute Formula.well_formed_formula?({p, q})
+  end
 end
